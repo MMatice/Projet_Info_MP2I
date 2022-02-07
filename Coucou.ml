a27485857035c313492d9e001d9e9f19981e07a3
Clement Salotti <salotticlement@gmail.com>
2022-02-07 15:41:36 +0100

    test_01

--- Coucou.ml
+++ Coucou.ml
@@ -1,8 +1,1 @@
-let affiche_des_trucs t =
-	for i = 0 to Array.length t - 1 do
-		Printf.printf "%d" t.(i)
-	done
-
-let _ =
-	let t = [|13; 15; 6; 6|] in
-	affiche_des_trucs t
+Printf.printf "Cocuou"
