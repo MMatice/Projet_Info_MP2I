57b48bfe552b51a72e11e762fcbe1b7649690fe5
Clement Salotti <salotticlement@gmail.com>
2022-02-08 18:06:37 +0100

    affichage dune 500x500

--- Dune.ml
+++ Dune.ml
@@ -9,15 +9,15 @@ let sky_color = rgb 52 146 235 (*COULEUR CIEL  *)
 
 
 let _ =
-    open_graph "1000x1000"; (*1000x1000 car c'est beau  *)
-    let screen = Array.make_matrix 1000 1000 Air in 
-    let hballe = ref 500 in (* Hauteur de départ de la balle et gerera la hauteur de la balle
+    open_graph "500x500"; (*500x500  *)
+    let screen = Array.make_matrix 500 500 Air in 
+    let hballe = ref 250 in (* Hauteur de départ de la balle et gerera la hauteur de la balle
 							   dans le temps*)
 	
 	
-    moveto 0 200; (*point de depart  *)
+    moveto 0 100; (*point de depart  *)
     set_color sky_color;
-    fill_rect 0 0 1000 1000; (*on fill un rectangle pour le ciel (background)  *)
+    fill_rect 0 0 500 500; (*on fill un rectangle pour le ciel (background)  *)
     Random.self_init(); (*seed random  *)
 
     for i = 0 to 4 do 
@@ -26,14 +26,14 @@ let _ =
     (*on joue alors sur les parametre pour ne pas avoir des choses trop bizarre  *)
     (*ça nous donne donc une line noir qui à l'allure d'une courbe  *)
         set_color 0;
-        let a = Random.int 200 + 50 + i in (*le i est pour décoré il sert à rien  *)
-        curveto (current_x(),Random.int 200 + 200) 
-        (current_x() + a, Random.int 200 + 300) 
+        let a = Random.int 100 + 25 + i in (*le i est pour décoré il sert à rien  *)
+        curveto (current_x(),Random.int 100 + 100) 
+        (current_x() + a, Random.int 100 + 150) 
         (current_x() + a ,current_y());
     (*on en fait deux d'affiler avec l'idée davoir concave convexe  *)
 
         curveto (current_x(),Random.int 200 - current_y()) 
-        (current_x() + a, current_y() - Random.int 100 - 100) 
+        (current_x() + a, current_y() - Random.int 50 - 50) 
         (current_x() + a ,current_y());
     done;
     set_color yellow;
@@ -52,7 +52,7 @@ let _ =
 
     while true do
         set_color green;
-        fill_circle 50 !hballe 10;
+        fill_circle 25 !hballe 5;
 		
 		let touche_sable = ref false in		(* sera utile plus tard normalement *)
 		
@@ -60,13 +60,13 @@ let _ =
 		(* Regarde si une touche du CLAVIER (d'autres fonction de la lib ne regarde que la souris)
 		   est presser et si c'est le cas la balle descend si elle touche du sable elle descend pas*)
 		let action = wait_next_event [Key_pressed] in
-		if (action.keypressed && screen.(50).(!hballe - 5) <> Sable)
+		if (action.keypressed && screen.(25).(!hballe - 5) <> Sable)
 		then begin
 			touche_sable := false;								
 			set_color sky_color;         (* Permet d'effacer *)
-			fill_circle 50 !hballe 10;   (* l'ancienne balle *)
+			fill_circle 25 !hballe 5;   (* l'ancienne balle *)
 			set_color green;			 (* Dessine la nouvelle *)
-			fill_circle 50 (!hballe - 2) 10;
-			hballe := !hballe - 2;
+			fill_circle 25 (!hballe - 1) 5;
+			hballe := !hballe - 1;
 			end;
     done
